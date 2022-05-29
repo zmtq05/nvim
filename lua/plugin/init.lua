b888ca50732f02ab0776dd4e3910c65faf8af165
zmtq05 <zmtq05@gmail.com>
2022-05-29 23:14:37 +0900

    add gitsigns

--- lua/plugin/init.lua
+++ lua/plugin/init.lua
@@ -3,10 +3,6 @@ local use = require("packer-tools.wrapper").new {
   inject_config = { enable = true, prefix = "plugin.config." },
 }
 return require("packer").startup(function(use_clean)
-  local treesitter = "nvim-treesitter/nvim-treesitter"
-  local devicons = "kyazdani42/nvim-web-devicons"
-  local plenary = "nvim-lua/plenary.nvim"
-  local telescope = "nvim-telescope/telescope.nvim"
 
   use_clean {
     "wbthomason/packer.nvim",
@@ -15,9 +11,15 @@ return require("packer").startup(function(use_clean)
 
   use_clean("~/repo/OneDark-Pro.nvim")
 
+  use_clean {
+    "kyazdani42/nvim-web-devicons",
+    "nvim-lua/plenary.nvim",
+  }
+
   use {
     "stevearc/dressing.nvim",
     "akinsho/toggleterm.nvim",
+    "lewis6991/gitsigns.nvim",
   }
 
   use_clean {
@@ -30,9 +32,10 @@ return require("packer").startup(function(use_clean)
   use {
     "nvim-neo-tree/neo-tree.nvim",
     branch = "v2.x",
-    requires = { plenary, devicons, "MunifTanjim/nui.nvim" },
+    requires = "MunifTanjim/nui.nvim",
   }
 
+  local treesitter = "nvim-treesitter/nvim-treesitter"
   use { treesitter, run = ":TSUpdate" }
 
   use { "windwp/nvim-autopairs", requires = treesitter }
@@ -46,7 +49,6 @@ return require("packer").startup(function(use_clean)
       "jose-elias-alvarez/null-ls.nvim",
       "folke/lua-dev.nvim",
       "j-hui/fidget.nvim",
-      plenary,
     },
   }
 
@@ -69,9 +71,9 @@ return require("packer").startup(function(use_clean)
   }
 
   use {
-    telescope,
+    "nvim-telescope/telescope.nvim",
     requires = {
       { "nvim-telescope/telescope-fzf-native.nvim", run = "make" },
-    }
+    },
   }
 end)
